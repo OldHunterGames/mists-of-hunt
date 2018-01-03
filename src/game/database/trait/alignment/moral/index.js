5e55fb751b9a72ee660c0c5a38208fbd794cfe5a
NicoBurno <nicoburno@gmail.com>
2018-01-03 10:37:50 +0700

    Trait - Alignment

--- src/game/database/trait/alignment/moral/index.js
+++ src/game/database/trait/alignment/moral/index.js
@@ -1,5 +1,7 @@
 // @flow
 
+import frequenciesRandom from 'utils/randomizers/frequencies';
+
 import type { Trait } from 'types/trait';
 
 export const GOOD: Trait = {
@@ -8,15 +10,31 @@ export const GOOD: Trait = {
     }
 };
 
+export const NEUTRAL: Trait = {};
+
 export const EVIL: Trait = {
     modifiers: {
         cunning: 1
     }
 };
 
+export const MORAL = {
+    entries: {
+        GOOD,
+        NEUTRAL,
+        EVIL
+    },
+    getRandom: frequenciesRandom([
+        [25, GOOD],
+        [50, NEUTRAL],
+        [25, EVIL]
+    ])
+};
+
 export default (next) => {
     next({
         GOOD: () => GOOD,
+        NEUTRAL: () => NEUTRAL,
         EVIL: () => EVIL
     });
 };
