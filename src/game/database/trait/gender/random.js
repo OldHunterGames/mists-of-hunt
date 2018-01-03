d142af160331341ab9d01d11cfd2a1e1212e876f
NicoBurno <nicoburno@gmail.com>
2018-01-03 09:59:23 +0700

    Traits generator fix

--- src/game/database/trait/gender/random.js
+++ src/game/database/trait/gender/random.js
@@ -1,35 +1,34 @@
 // @flow
 
-
 import frequenciesRandom from 'utils/randomizers/frequencies';
 
 import { FAY } from 'trait/race';
-import { MALE, FEMALE, FUTANARI } from 'trait/gender';
 
-import type { Trait } from 'types/trait';
+import type { Trait, TraitsMap } from 'types/trait';
 
 const interestRaces = [FAY];
 
-const fayGender = frequenciesRandom([
-    [90, FEMALE],
-    [15, FUTANARI],
-    [5, MALE]
-]);
+export default ({ MALE, FEMALE, FUTANARI }: TraitsMap) => {
+    const fayGender = frequenciesRandom([
+        [80, FEMALE],
+        [15, FUTANARI],
+        [5, MALE]
+    ]);
+    const defaultGender = frequenciesRandom([
+        [45, MALE],
+        [45, FEMALE],
+        [10, FUTANARI]
+    ]);
 
-const defaultGender = frequenciesRandom([
-    [45, MALE],
-    [45, FEMALE],
-    [10, FUTANARI]
-]);
+    return (traits: Array<Trait>) => {
+        const race = traits.find((t) => {
+            return interestRaces.some((r) => t === r);
+        });
 
-export default (traits: Array<Trait>) => {
-    const race = traits.find((t) => {
-        return interestRaces.some((r) => t === r);
-    });
+        if (race === FAY) {
+            return fayGender();
+        }
 
-    if (race === FAY) {
-        return fayGender();
-    }
-
-    return defaultGender();
+        return defaultGender();
+    };
 };
